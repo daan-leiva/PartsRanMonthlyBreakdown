628ada2f03589c815bbf3e11db30a7fb1e488ea3
Daan Leiva <daan.leiva@gmail.com>
2016-04-08 12:39:34 -0700

    ADded the parts detail query. Ready for testing

--- PartsRanMonthlyBreakdown/PartDetail.cs
+++ PartsRanMonthlyBreakdown/PartDetail.cs
@@ -18,32 +18,29 @@ namespace PartsRanMonthlyBreakdown
         private static string password = "Cloudy2Day";
         string connection_string = "DSN=" + DSN + ";UID=" + userName + ";PWD=" + password;
 
-        public PartDetail()
+        int month;
+        int year;
+
+        public PartDetail(int month, int year)
         {
             InitializeComponent();
+
+            // set up variables
+            this.month = month;
+            this.year = year;
         }
 
         private void PartDetail_Load(object sender, EventArgs e)
         {
-            string query = "SELECT SUM(CASE WHEN (CAST(jT.Work_Date AS DATETIME) >= CAST('20150401 00:00:00.000' AS DATETIME) AND CAST(jT.Work_Date AS DATETIME) < CAST('20150501 00:00:00.000' AS DATETIME)) THEN jT.Act_Run_Qty ELSE 0 END) AS April_15,\n" +
-                                    "\tSUM(CASE WHEN (CAST(jT.Work_Date AS DATETIME) >= CAST('20150501 00:00:00.000' AS DATETIME) AND CAST(jT.Work_Date AS DATETIME) < CAST('20150601 00:00:00.000' AS DATETIME)) THEN jT.Act_Run_Qty ELSE 0 END) AS May_15,\n" +
-                                    "\tSUM(CASE WHEN (CAST(jT.Work_Date AS DATETIME) >= CAST('20150601 00:00:00.000' AS DATETIME) AND CAST(jT.Work_Date AS DATETIME) < CAST('20150701 00:00:00.000' AS DATETIME)) THEN jT.Act_Run_Qty ELSE 0 END) AS June_15,\n" +
-                                    "\tSUM(CASE WHEN (CAST(jT.Work_Date AS DATETIME) >= CAST('20150701 00:00:00.000' AS DATETIME) AND CAST(jT.Work_Date AS DATETIME) < CAST('20150801 00:00:00.000' AS DATETIME)) THEN jT.Act_Run_Qty ELSE 0 END) AS July_15,\n" +
-                                    "\tSUM(CASE WHEN (CAST(jT.Work_Date AS DATETIME) >= CAST('20150801 00:00:00.000' AS DATETIME) AND CAST(jT.Work_Date AS DATETIME) < CAST('20150901 00:00:00.000' AS DATETIME)) THEN jT.Act_Run_Qty ELSE 0 END) AS August_15,\n" +
-                                    "\tSUM(CASE WHEN (CAST(jT.Work_Date AS DATETIME) >= CAST('20150901 00:00:00.000' AS DATETIME) AND CAST(jT.Work_Date AS DATETIME) < CAST('20151001 00:00:00.000' AS DATETIME)) THEN jT.Act_Run_Qty ELSE 0 END) AS September_15,\n" +
-                                    "\tSUM(CASE WHEN (CAST(jT.Work_Date AS DATETIME) >= CAST('20151001 00:00:00.000' AS DATETIME) AND CAST(jT.Work_Date AS DATETIME) < CAST('20151101 00:00:00.000' AS DATETIME)) THEN jT.Act_Run_Qty ELSE 0 END) AS October_15,\n" +
-                                    "\tSUM(CASE WHEN (CAST(jT.Work_Date AS DATETIME) >= CAST('20151101 00:00:00.000' AS DATETIME) AND CAST(jT.Work_Date AS DATETIME) < CAST('20151201 00:00:00.000' AS DATETIME)) THEN jT.Act_Run_Qty ELSE 0 END) AS November_15,\n" +
-                                    "\tSUM(CASE WHEN (CAST(jT.Work_Date AS DATETIME) >= CAST('20151201 00:00:00.000' AS DATETIME) AND CAST(jT.Work_Date AS DATETIME) < CAST('20160101 00:00:00.000' AS DATETIME)) THEN jT.Act_Run_Qty ELSE 0 END) AS December_15,\n" +
-                                    "\tSUM(CASE WHEN (CAST(jT.Work_Date AS DATETIME) >= CAST('20160101 00:00:00.000' AS DATETIME) AND CAST(jT.Work_Date AS DATETIME) < CAST('20160201 00:00:00.000' AS DATETIME)) THEN jT.Act_Run_Qty ELSE 0 END) AS January_16,\n" +
-                                    "\tSUM(CASE WHEN (CAST(jT.Work_Date AS DATETIME) >= CAST('20160201 00:00:00.000' AS DATETIME) AND CAST(jT.Work_Date AS DATETIME) < CAST('20160301 00:00:00.000' AS DATETIME)) THEN jT.Act_Run_Qty ELSE 0 END) AS February_16,\n" +
-                                    "\tSUM(CASE WHEN (CAST(jT.Work_Date AS DATETIME) >= CAST('20160301 00:00:00.000' AS DATETIME) AND CAST(jT.Work_Date AS DATETIME) < CAST('20160401 00:00:00.000' AS DATETIME)) THEN jT.Act_Run_Qty ELSE 0 END) AS March_16,\n" +
-                                    "\tSUM(CASE WHEN (CAST(jT.Work_Date AS DATETIME) >= CAST('20160401 00:00:00.000' AS DATETIME) AND CAST(jT.Work_Date AS DATETIME) < CAST('20160501 00:00:00.000' AS DATETIME)) THEN jT.Act_Run_Qty ELSE 0 END) AS April_16\n" +
+            string query = "SELECT jJ.Part_Number AS \"Part Number\", SUM(jT.Act_Run_Qty) AS \"Run Qty\"\n" +
                             "FROM PRODUCTION.dbo.Job_Operation AS jO\n" +
                             "LEFT JOIN PRODUCTION.dbo.Job_Operation_Time AS jT\n" +
                             "ON jO.Job_Operation = jT.Job_Operation\n" +
                             "LEFT JOIN PRODUCTION.dbo.Job AS jJ\n" +
                             "ON jO.Job = jJ.Job\n" +
-                            "WHERE jT.Act_Run_Qty <> 0;";
+                            "WHERE jT.Act_Run_Qty <> 0 AND CAST(jT.Work_Date AS DATETIME) >= CAST('" + year + "" + month.ToString("00") + "01 00:00:00.000' AS DATETIME) AND CAST(jT.Work_Date AS DATETIME) < CAST('"+year +(month == 1 ? 1 : 0)+"0501 00:00:00.000' AS DATETIME)\n" +
+                            "GROUP BY jJ.Part_Number\n" +
+                            "ORDER BY Part_Number;";
 
             using (OdbcConnection conn = new OdbcConnection(connection_string))
             using (OdbcDataAdapter adapter = new OdbcDataAdapter(query, conn))
@@ -52,8 +49,6 @@ namespace PartsRanMonthlyBreakdown
                 adapter.Fill(table);
 
                 dataGridView.DataSource = table;
-
-
             }
         }
     }
